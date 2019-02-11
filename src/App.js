4edebc5a762eb5440484f31136c01bd035a64d3c
cristin <cristin.herrera.guerra@gmail.com>
2019-02-11 09:21:52 -0500

    implementando incrementar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contador: 0
+    }
+  }
+
+  incrementar() {
+    this.setState({
+      contador: this.state.contador + 1
+    })
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.contador}</span>
+      <button  onClick={this.incrementar.bind(this)} id="inc">Incrementa</button>
       </div>
     );
   }
